f9f9f6fbdb705a24e58b0be623a2ac05a18e380b
AlexandrValgamov <valgamov2010@yandex.ru>
2024-01-09 17:31:14 +0500

    feat: add cors, logger

---
+++ middlewares/cors.js
@@ -0,0 +1,19 @@
+const { allowedCors, DEFAULT_ALLOWED_METHODS } = require('../utils/constants');
+
+const corsHandler = (req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  next();
+};
+
+module.exports = corsHandler;
